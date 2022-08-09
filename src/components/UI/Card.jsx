87bafed9ead1246c6d83374fa231e8d0cd2395e3
Peter Huynh <peterlonghaihuynh25@gmail.com>
2022-08-09 17:51:25 -0400

    add event handlers, set and reset States

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import classes from './Card.module.css';
+import classes from "./Card.module.css";
 
 const Card = (props) => {
-    return (
-        <div className={`${classes.card} ${props.className}`}>{props.children}</div>
-    );
-}
+  return (
+    // Everything inside <Card> tag in AddUser.jsx are children of props of this Card file
+    // {classes.card} apply css class from this file css import (this case is Card.module.css)
+    // {props.className} apple css class from wherever the component is (this case AddUser.module.css)
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+  );
+};
 
-export default Card
+export default Card;
--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,35 +1,53 @@
 import React, { useState } from "react";
-import Card from '../UI/Card';
-import Button from '../UI/Button';
-import classes from './AddUser.module.css';
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
 
-    const addUserHandler = (event) => {
-        event.preventDefault();
-    };
+  // when submit button is clicked (onSubmit event), function will be called
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0)
+      return;
+    if (+enteredAge < 1) return;
+    // Reset inputs (by feeding current States to <input> using "value")
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
 
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    };
+  // With every key stroke event triggers (onChange), function will be called
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  // With every key stroke event triggers (onChange), function will be called
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
 
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    };
-
-    return (
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text" onChange={usernameChangeHandler} />
-                <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" onChange={ageChangeHandler} />
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
-    );
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
 };
 
-export default AddUser
+export default AddUser;
