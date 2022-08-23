2a211d42296a3ef9d6389d603beb589945c594b5
Peter Huynh <70732959+peter25316@users.noreply.github.com>
2022-08-23 18:16:55 -0400

    Update AddUser.jsx

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
@@ -6,9 +6,14 @@ import ErrorModal from "../UI/ErrorModal";
 import Wrapper from "../Wrapper/Wrapper";
 
 const AddUser = (props) => {
+  // When the "submit" button is clicked, ref will assigned espected element 
+  // to these two varibles. In this case they will be the two "input"s
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   // Username and age States
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  // const [enteredUsername, setEnteredUsername] = useState("");
+  // const [enteredUserAge, setEnteredUserAge] = useState("");
   // Error State
   const [error, setError] = useState();
 
@@ -16,8 +21,12 @@ const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
 
+    // Collect the values of the two refs
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+
     // Set error
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age (non-empty values).",
@@ -34,21 +43,28 @@ const AddUser = (props) => {
     }
 
     // Use onAddUser property of props (which calls addUserHandler function of App component)
-    props.onAddUser(enteredUsername, enteredAge);
+    props.onAddUser(enteredName, enteredAge);
 
     // Reset inputs (by feeding current State to <input> using "value")
-    setEnteredUsername("");
-    setEnteredAge("");
+    // setEnteredUsername("");
+    // setEnteredUserAge("");
+
+    // Reseting input using ref
+    // This is DOM manipulation so it is VERY RARELY used 
+    // In our case it is fine because we only read data and we don't need
+    // change anything.
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
-  // With every key stroke event triggers (onChange), function will be called
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-  // With every key stroke event triggers (onChange), function will be called
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // // With every key stroke event triggers (onChange), function will be called
+  // const usernameChangeHandler = (event) => {
+  //   setEnteredUsername(event.target.value);
+  // };
+  // // With every key stroke event triggers (onChange), function will be called
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const errorHandler = () => {
     setError(null);
@@ -70,15 +86,17 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            // value={enteredUsername}
+            // onChange={usernameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            // value={enteredUserAge}
+            // onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
